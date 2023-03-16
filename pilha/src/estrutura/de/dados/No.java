48bc4788f71d381134f62562307835feb436f62a
jennifer cristine <107707471+jennifer-cristine12@users.noreply.github.com>
2023-03-16 11:33:13 -0300

    Add files via upload

---
+++ pilha/src/estrutura/de/dados/No.java
@@ -0,0 +1,14 @@
+
+package estrutura.de.dados;
+
+
+public class No {
+    Object info;
+    No ponteiro;
+
+    public No(Object info) {
+        this.info = info;
+        this.ponteiro = null;
+    }
+
+}
---
+++ pilha/src/estrutura/de/dados/Principal.java
@@ -0,0 +1,6 @@
+
+package estrutura.de.dados;
+
+public class Principal {
+    
+}
