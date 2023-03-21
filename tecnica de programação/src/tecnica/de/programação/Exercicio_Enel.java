011456a2ab71acd01c75772d8208bc04fb28e8ad
jennifer cristine <107707471+jennifer-cristine12@users.noreply.github.com>
2023-03-21 11:19:29 -0300

    Add files via upload

--- tecnica de programação/src/tecnica/de/programação/Exercicio_Enel.java
+++ tecnica de programação/src/tecnica/de/programação/Exercicio_Enel.java
@@ -7,9 +7,17 @@ public class Exercicio_Enel {
     public static void main(String[] args) {
     double consumo, fornecimento, icms, cofins, pispasesp,icmscofins, icmpispasesp, fatura  ;
     consumo = Double.parseDouble(JOptionPane.showInputDialog(null,"informe o consumo"));
-   
+    fornecimento = consumo*0.28172;
     
-    
-  
+    if(consumo<=200){
+        icms = fornecimento * 0.136363;
+        cofins = 0.0614722 * fornecimento;
+    }else{
+        icms = fornecimento*0.333333;
+        cofins = 0.0730751 *fornecimento;
+        
+    }
+    fatura = fornecimento+icms+cofins+pispasesp+icmcofins+icmpispasesp;
+    JOptionPane.showMessageDialog(null,"o fornecimento equivale a: "+((String.format("%.2f", fornecimento)))+"\no icms equivale a: "+((String.format("%.2f", icms))));
 }
 }
